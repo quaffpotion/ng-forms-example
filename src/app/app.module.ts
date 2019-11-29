cb368710e287d125f02422a5547c995e084491d7
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-11-29 12:55:26 -0600

    Adds reactive forms and a boilerplate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
+//Added:
+import { ReactiveFormsModule } from "@angular/forms";
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NameEditorComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
