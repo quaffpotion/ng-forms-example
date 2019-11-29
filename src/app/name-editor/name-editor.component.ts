06a55ea01922ba765d337e948819d0fa505a6e78
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-11-29 13:12:32 -0600

    minimal reactive form using FormControl

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-name-editor',
-  templateUrl: './name-editor.component.html',
-  styleUrls: ['./name-editor.component.css']
+  selector: "app-name-editor",
+  template: `
+    <label>
+      Input Name:
+      <input type="text" [formControl]="name" />
+    </label>
+    <p>Name: {{ name.value }}</p>
+    <button (click)="reset()">Reset</button>
+  `,
+  styleUrls: ["./name-editor.component.css"]
 })
 export class NameEditorComponent implements OnInit {
+  name = new FormControl(""); // set initial value to the empty string
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  reset() {
+    this.name.setValue("Nancy Pelosi");
   }
-
 }
